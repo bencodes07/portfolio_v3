7d31e2e2cf5eea603de6fa8040e8c1cf316233f2
Ben <boeckmannben@gmail.com>
2024-07-31 10:51:52 +0100

    build: eslint error fixed

--- src/components/projects/Curve.tsx
+++ src/components/projects/Curve.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { motion, useAnimationControls } from "framer-motion";
 import { useScroll } from "../../contexts/ScrollContext";
 
