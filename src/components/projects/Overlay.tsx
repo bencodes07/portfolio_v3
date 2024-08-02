a25af63b97247fbe79891bcb41c614feb0097c3b
Ben Böckmann <boeckmannben@gmail.com>
2024-08-02 18:19:42 +0100

    Added Project Overlay design

--- src/components/projects/Overlay.tsx
+++ src/components/projects/Overlay.tsx
@@ -4,13 +4,13 @@ import { motion } from "framer-motion";
 export default function Overlay({ project }: { project: Project }) {
   return (
     <motion.div
-      className="text-white pointer-events-auto h-screen max-h-screen flex max-w-[1000px] w-full flex-col justify-between items-start"
+      className="text-white px-4 pointer-events-auto h-screen max-h-screen flex max-w-[1000px] w-full flex-col justify-between items-start"
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 20 }}
       transition={{ duration: 0.4 }}
     >
-      <h1 className="khula-regular text-8xl tracking-[calc(6rem * 0.03)] mb-[4vh] mt-[20vh]">
+      <h1 className="khula-regular max-sm:text-[12vw] text-8xl tracking-[calc(6rem * 0.03)] mb-[4vh] mt-[20vh]">
         {project.title}
       </h1>
 
@@ -18,13 +18,13 @@ export default function Overlay({ project }: { project: Project }) {
         Description
       </p>
       <hr className="w-[350px] border-gray-2 mt-2" />
-      <p className="poppins-regular text-base text-gray-1 mt-8 mb-16 w-[800px]">
+      <p className="poppins-regular text-base text-gray-1 mt-8 mb-16 max-w-[800px] w-screen">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
         libero et velit interdum, ac aliquet odio mattis. Class aptent taciti
         sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
       </p>
       <img
-        className="w-full object-cover object-top h-[45vh] rounded-2xl rounded-b-none"
+        className="w-full object-cover object-top h-[45vh] rounded-2xl rounded-b-none select-none"
         style={{
           border: "1px solid rgb(" + project.color + ")",
           borderBottom: "none",
