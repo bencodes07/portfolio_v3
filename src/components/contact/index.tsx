84217ff382e84d6373f719d245ed408add890d7e
Ben Böckmann <75972296+bencodes07@users.noreply.github.com>
2025-11-15 16:50:20 +0100

    Update index.tsx

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,5 +1,5 @@
 import { MotionValue, motion, useAnimationControls } from "framer-motion";
-import { Linkedin, Mail, Phone } from "lucide-react";
+import { Linkedin, Mail } from "lucide-react";
 import { useEffect, useState } from "react";
 import Magnetic from "../Magnetic";
 
